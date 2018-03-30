d78df2e2a67efcf5d5af0f2307c87a4ddcc6bc1f
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2018-03-30 16:59:21 +0300

    Disable trello

--- cmd/single-process-mode/main.go
+++ cmd/single-process-mode/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/requilence/integram"
 	// "github.com/integram-org/trello"
 	"github.com/integram-org/gitlab"
-	"github.com/kelseyhightower/envconfig"
+	// "github.com/kelseyhightower/envconfig"
 )
 
 func main() {
